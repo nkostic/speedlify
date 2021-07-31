de6d5630bda560598f5dc7b865d8cb27f146efb9
Nenad Kostic <nenadkostic@gmail.com>
2021-07-31 14:23:35 -0600

    Add Nenad

---
+++ _data/sites/nenadkostic.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: "nenadkostic.com", // optional, falls back to object key
+	description: "Nenad's Personal web site",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
+	},
+	urls: [
+		"https://nenadkostic.com/",
+		"https://nenadkostic.com/blog/new-beginnings",
+		"https://nenadkostic.com/blog/new-site"
+	]
+};
+
+
